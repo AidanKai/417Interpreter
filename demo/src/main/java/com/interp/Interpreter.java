0f139a079ced88fe5137c9174ad950fcca93e9b9
aknelso2 <aknelso2@ncsu.edu>
2024-11-07 15:08:21 -0500

    Initial maven project skeleton supporting imports for Gson library

---
+++ demo/src/main/java/com/interp/Interpreter.java
@@ -0,0 +1,15 @@
+package com.interp;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class Interpreter
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ demo/src/test/java/com/interp/InterpreterTest.java
@@ -0,0 +1,16 @@
+package com.interp;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class InterpreterTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
